13ad1ead85621c5275c6b4143cf40343319184bd
Madeleine Belanger <madeleinepbelanger@gmail.com>
2022-05-23 19:47:21 -0700

    update

--- models/ComfortZone.js
+++ models/ComfortZone.js
@@ -13,7 +13,7 @@ const ComfortZone = new mongoose.Schema({
   //   type: Date,
   //   required: true,
   // },
-  
+   
 
 });  
 
@@ -21,4 +21,3 @@ const ComfortZone = new mongoose.Schema({
 
 module.exports = mongoose.model("AskTheUniverse", AskTheUniverseSchema);
 
- 
