e982656889ec36d384b85c1298a3362b6fad278c
madeleinebelanger <madeleinepbelanger@gmail.com>
2022-09-25 11:16:59 -0700

    update

--- routes/posts.js
+++ routes/posts.js
@@ -13,8 +13,8 @@ router.put("/likePost/:id", postsController.likePost);
 
 router.delete("/deletePost/:id", postsController.deletePost);
 
-router.get('/getGratitude', ensureAuth, postsController.getPost)
-router.post('/createGratitude', ensureAuth, postsController.createPost)
+router.get('/getGratitude', ensureAuth, postsController.getGratitude)
+router.post('/createGratitude', ensureAuth, postsController.createGratitude)
 
 
 
