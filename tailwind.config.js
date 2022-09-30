cb7b26bc3fe91d76e9ef6285393aa33f67e682e7
Madeleine Belanger <madeleinepbelanger@gmail.com>
2022-09-30 10:07:01 -0700

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,10 @@
 module.exports = {
-  content: ["./views/**/*.ejs"],
+  content: ["./views/**/*ejs"],
   theme: {
     extend: {
       colors: {
         gray: "##6b7280",
-        brandRed: "#FF3F3F",
-        brandYellow: "#FCBF49",
-        brandBlue: "#2B3A67",
-        brandGreen: "#00916E",
-        faint: "#959DB3",
-        gradesA: "#02B608",
-        gradesB: "#8FD100",
-        gradesC: "#FFE920",
-        gradesD: "#FF8503",
-        gradesF: "#FC0101",
+        brown:'#854d0e'
       },
       fontFamily: {
         "mainText": ["Fredoka", "sans-serif"],
