bb18a5b981cf64bbab1d3ba47412f91333abfe1c
Madeleine Belanger <madeleinepbelanger@gmail.com>
2022-12-09 15:53:16 -0800

    update

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,7 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
-const path = require("path")
-const Gratitude= require('../models/Gratitude')
+const path = require("path");
+const Gratitude = require("../models/Gratitude");
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -9,7 +9,11 @@ module.exports = {
       const posts = await Post.find({ user: req.user.id });
       const gratitudeLog = await Gratitude.find({ userId: req.user.id });
 
-      res.render("profile.ejs", { posts: posts, user: req.user, gratitudeLog:gratitudeLog });
+      res.render("profile.ejs", {
+        posts: posts,
+        user: req.user,
+        gratitudeLog: gratitudeLog,
+      });
     } catch (err) {
       console.log(err);
     }
@@ -37,7 +41,7 @@ module.exports = {
       // Upload image to cloudinary
       const result = await cloudinary.uploader.upload(req.file.path);
 
-      let newPost= await Post.create({
+      let newPost = await Post.create({
         title: req.body.title,
         image: result.secure_url,
         cloudinaryId: result.public_id,
@@ -45,49 +49,45 @@ module.exports = {
         likes: 0,
         user: req.user.id,
       });
-      console.log(newPost._id)
+      console.log(newPost._id);
       res.redirect(`/post/addDescription/${newPost._id}`);
     } catch (err) {
       console.log(err);
     }
   },
 
-
-
   editPost: async (req, res) => {
+    console.log(req);
     try {
-      // Upload image to cloudinary
-      const post = await Post.findById(req.params.id)
-
-      post.title= req.body.title,
-      post.caption=req.body.caption,
-
-      console.log(newPost._id)
-      res.redirect('/visionBoard');
+      await Post.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          title: req.body.title,
+          caption: req.body.caption,
+        }
+      );
+      console.log(`post ${req.params.id} has been updated!`);
+      res.redirect("/visionBoard");
     } catch (err) {
       console.log(err);
     }
   },
 
-
-  createImageDescription: async (req, res)=>{
-    try{
-        await Gratitude.create({
-          gratitudeItem1: req.body.item,
-          userId: req.user.id,
-          date: new Date(),   
-
-        });
-        console.log('Your gratitude for today has been logged!')
-        res.redirect('/visionBoard')
-        console.log(req.body.item)
-        console.log(req.user.id)
-    }catch(err){
-        console.log(err)
+  createImageDescription: async (req, res) => {
+    try {
+      await Gratitude.create({
+        gratitudeItem1: req.body.item,
+        userId: req.user.id,
+        date: new Date(),
+      });
+      console.log("Your gratitude for today has been logged!");
+      res.redirect("/visionBoard");
+      console.log(req.body.item);
+      console.log(req.user.id);
+    } catch (err) {
+      console.log(err);
     }
-},
-
-
+  },
 
   getEditDescription: async (req, res) => {
     try {
@@ -98,17 +98,13 @@ module.exports = {
     }
   },
 
-
-
-getCreatePostPage: async (req,res) => {
-  try {
-      res.render('createPost.ejs', {user: req.user})
-  }
-  catch(err) {
-      console.log(err)
-  }
-},
-
+  getCreatePostPage: async (req, res) => {
+    try {
+      res.render("createPost.ejs", { user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
 
   likePost: async (req, res) => {
     try {
@@ -125,13 +121,11 @@ getCreatePostPage: async (req,res) => {
     }
   },
 
-
-
   deletePost: async (req, res) => {
     try {
       // Find post by id
       let post = await Post.findById({ _id: req.params.id });
-      
+
       // Delete image from cloudinary
       await cloudinary.uploader.destroy(post.cloudinaryId);
       // Delete post from db
@@ -143,42 +137,20 @@ getCreatePostPage: async (req,res) => {
     }
   },
 
-
-
-getAffirmationsPage: async (req, res) => {    
+  getAffirmationsPage: async (req, res) => {
     try {
       res.render("affirmations.ejs");
-    
-    } 
-    catch (err) {
+    } catch (err) {
       res.redirect("/affirmations");
     }
   },
 
-
-
-
-
-getAffirmation:('/api/',(request,response)=>{
-
-    const affirmationCategory= request.params.affirmationCategory
-    if(affirmations[affirmationCategory]){
-        response.json(affirmations[affirmationCategory])
-    }
-}),
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+  getAffirmation:
+    ("/api/",
+    (request, response) => {
+      const affirmationCategory = request.params.affirmationCategory;
+      if (affirmations[affirmationCategory]) {
+        response.json(affirmations[affirmationCategory]);
+      }
+    }),
+};
