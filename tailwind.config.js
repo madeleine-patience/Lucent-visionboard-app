10879bc1fd0f3ea51313879905462de70bd713a4
Madeleine Belanger <madeleinepbelanger@gmail.com>
2022-12-12 22:27:34 -0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,32 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
-
+// tailwind.config.js
 
 module.exports = {
   content: ["./views/**/*.ejs"],
   theme: {
     extend: {
       colors: {
-        gray: "##6b7280",
-        brown:'#854d0e'
+        primary: '#32a852',
+        secondary: '#ff0015'
       },
       fontFamily: {
-        'sans': ['Graphik', ...defaultTheme.fontFamily.sans],
-        serif: ['Merriweather', 'serif'],
+        'press-start': ['"Press Start 2P"', 'cursive'],
+      },
     },
-    },
-  },
-  plugins: [require("@tailwindcss/forms")],
-}
 
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem'
+    }
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+  }
+}
